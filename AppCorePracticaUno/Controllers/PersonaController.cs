09e9199288bf6aa38f0c53be21127f12bf5b930d
Kemberly-Amaya <as0021032020@unab.edu.sv>
2021-09-24 09:39:54 -0600

    AppCore
    Primera App con .NET CORE

---
+++ AppCorePracticaUno/Controllers/PersonaController.cs
@@ -0,0 +1,41 @@
+﻿using AppCorePracticaUno.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppCorePracticaUno.Controllers
+{
+    public class PersonaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Index(Persona persona)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Registro");
+            }
+            else
+            {
+                return View("Index", persona);
+                
+            }
+        }
+
+        public IActionResult Registro()
+        {
+            return View();
+        }
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
